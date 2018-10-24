4df1e5d5ca6a234548a39f246a4cb53ecafc413e
rezerbit <rezerbit@gmail.com>
2018-10-24 20:26:56 +0500

    add redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [ENV['ALLOWED_REQUEST_ORIGINS']]
 end
