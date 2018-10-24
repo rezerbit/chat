5278a4723519af98ba72017642a14338d54c557a
rezerbit <rezerbit@gmail.com>
2018-10-24 20:47:08 +0500

    fix

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -2,7 +2,7 @@
 echo "tag: "${1:-latest}
 cd ..
 
-docker build -t registry.rezerbit.com/chat/app:${1:-latest} -f docker/app/Dockerfile.production .
-docker push registry.rezerbit.com/chat/app:${1:-latest}
+docker build -t registry.rezerbit.com/chat/production/app:${1:-latest} -f docker/app/Dockerfile.production .
+docker push registry.rezerbit.com/chat/production/app:${1:-latest}
 
 echo 'success!---------------------------------------------------------------------------'
