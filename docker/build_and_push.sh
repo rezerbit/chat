a4ec1f11454e9804428c06ca3253dc273c9d13a8
rezerbit <rezerbit@gmail.com>
2018-10-25 14:56:00 +0500

    f

--- docker/build_and_push.sh
+++ docker/build_and_push.sh
@@ -2,7 +2,7 @@
 echo "tag: "${1:-latest}
 cd ..
 
-docker build -t registry.rezerbit.com/chat/production/app:${1:-latest} -f docker/app/Dockerfile.production .
+docker build --no-cache -t registry.rezerbit.com/chat/production/app:${1:-latest} -f docker/app/Dockerfile.production .
 docker push registry.rezerbit.com/chat/production/app:${1:-latest}
 
 echo 'success!---------------------------------------------------------------------------'
